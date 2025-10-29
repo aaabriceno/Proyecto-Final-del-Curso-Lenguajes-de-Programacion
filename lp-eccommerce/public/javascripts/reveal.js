5a1896fab1751cd3b17cdb2c25f917ba937bf9ec
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2025-10-28 23:32:34 -0500

    Empezando todo desde 0

--- lp-eccommerce/public/javascripts/reveal.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('scroll', () => {
-  document.querySelectorAll('.reveal').forEach(el => {
-    const y = el.getBoundingClientRect().top;
-    if (y < window.innerHeight * 0.85) el.classList.add('on');
-  });
-});
