70d4c9fc2bc1df9d378e3c5de5372d611b41e992
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2025-10-28 23:32:34 -0500

    Empezando todo desde 0

--- lp-eccommerce/app/models/Cart.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-case class CartItem(product: Product, qty: Int) {
-  def subtotal: BigDecimal = product.price * qty
-}
-
-case class Cart(items: Seq[CartItem]) {
-  def total: BigDecimal = items.map(_.subtotal).sum
-}
